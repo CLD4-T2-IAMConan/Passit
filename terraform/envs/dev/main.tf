274bc203952379e2cdc74e12612f019161f2c0b1
daeunkim701 <lionboot2025@gmail.com>
2025-12-17 14:45:42 +0900

    feat: envs/dev main, variables

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,1 +1,17 @@
 # Dev Environment - Module Calls
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "eks" {
+  project_name       = "passit"
+  source             = "../../modules/eks"
+  vpc_id             = var.vpc_id
+  private_subnet_ids = var.private_subnet_ids
+  cluster_name       = "passit-dev"
+  environment        = "dev"
+}
--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -1,1 +1,17 @@
 # Dev Environment Variables
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
